9cf505e01612455dcaf87fd7b8fbd456af41783e
Rigo vilchez <rigoalejandrovilchezsandrea@gmail.com>
2025-06-18 02:09:42 -0400

    resubido

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,15 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    #...
+    result = result.upper()
+    
+    replacements = {
+        "O": "0",
+        "I": "1",
+        "A": "@"
+    }
+    for old, new in replacements.items():
+        result = result.replace(old, new)
+    
+    result = list(result)
     return result  
